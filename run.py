1b2adf5ba736c38d3f33cc27b21b2c500eb0d11e
Alex Nørgaard <umbra@abstractumbra.dev>
2024-06-11 10:54:48 +0100

    pass in correct ips

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ import uvicorn
 
 if __name__ == "__main__":
     host = "127.0.0.1" if os.getenv("RTFS") else "0.0.0.0"
-    conf = uvicorn.Config("rtfs:APP", host=host, port=8030, workers=5)
+    conf = uvicorn.Config("rtfs:APP", host=host, port=8030, workers=5, proxy_headers=True, forwarded_allow_ips="*")
     server = uvicorn.Server(conf)
 
     server.run()
